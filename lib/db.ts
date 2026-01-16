c61df7a040906cd74e6c2ebab9897451726d0766
Siam <siamthelegendman@gmail.com>
2026-01-16 23:23:31 +0600

    Connection to the database has created

---
+++ lib/db.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI
+
+if(!MONGO_URI) {
+    throw new Error("Please define mongo URI in the .env")
+}
+
+let cached = global.mongoose
+
+if(!cached){
+    cached= global.mongoose = {conn: null , promise: null}
+}
+
+export async function connetToDatabase() {
+    if(cached.conn) {
+        return cached.conn
+    }
+    
+    if(!cached.promise){
+        const opt = {
+            bufferCommands :true,
+            maxPoolSize: 10
+        }
+        mongoose.connect(MONGO_URI , opt).then(()=> mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw error
+    }
+}
